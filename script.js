f11e50a24269318f3618af324aa69345426b2149
Ben Matheson <mathesonben@gmail.com>
2023-08-02 05:26:22 -0800

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVubWF0aGVzb24iLCJhIjoiY2xranI1bWoxMHRhejNrbnI4ZXJiemM0eSJ9.j1-iyMOIWgRx7aKfVpRTGQ';
+
+const map = new mapboxgl.Map({
+  
+  
+  container: "map",
+  style:'mapbox://styles/mapbox/streets-v12',
+  center: [-166.77963727933212,  68.34953457734584],
+  
+  zoom: 9
+  
+})
